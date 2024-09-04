5e291b35dfff5a733a3b38e7005a36cc2ade8b2b
RohitBasantYadav <yadav.rohit.basant@gmail.com>
2024-09-04 14:58:51 +0530

    resolved small issue

--- backend/controllers/userControllers/userProfile.controller.js
+++ backend/controllers/userControllers/userProfile.controller.js
@@ -3,7 +3,7 @@ const UserModel = require("../../models/user.model")
 const profile = async(req,res)=>{
     const {userId} = req.body;
     try {
-        const user = await UserModel.findOne(userId);
+        const user = await UserModel.findOne({_id:userId});
         if(user){
             return res.status(200).json({msg:`User Profile:`,name:user.name,email:user.email,_id:user._id});
         }
